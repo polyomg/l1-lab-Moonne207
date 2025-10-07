1176f99eba3640f140fd23da35e478ae868a18a6
Moonne207 <anh749681@gmail.com>
2025-10-07 13:44:05 +0700

    TV00071_Chuquocanh_lab4

--- Lab4/src/test/java/poly/edu/tv00103_lab4/Tv00103Lab4ApplicationTests.java
+++ Lab4/src/test/java/poly/edu/tv00103_lab4/Tv00103Lab4ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.poly.lab4;
+package poly.edu.tv00103_lab4;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Lab4ApplicationTests {
+class Tv00103Lab4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
